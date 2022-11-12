fb04c3eade717d7930a50ff1eb53b106f91916d2
M V K <krishna.mugala9@gmail.com>
2022-11-12 14:30:08 +0530

    changes made

--- Keep-Note/src/app/model/notes.ts
+++ Keep-Note/src/app/model/notes.ts
@@ -2,8 +2,8 @@ import { Note } from './note';
 
 export const Notes:Note[]=[
     {id:1, title:"sample-note", content:"This is a sample note added for testing purpose"},
-    {id:2, title:"Practice-exercise:typescript", content:"to add function to calculate final bill amount"},
-    {id:3, title:"challenge: typescript", content:"to add function to fetch notes"},
-    {id:4, title:"refactor practice exercise", content:"code needs to be well-indented"},
-    {id:5, title:"refactor challenge", content:"make design responsive and add comments in code"}
+    {id:2, title:"Adding data", content:"to add function to calculate final bill amount"},
+    {id:3, title:"Fetch Data", content:"to add function to fetch notes"},
+    {id:4, title:"vamshi", content:"code needs to be well-indented"},
+    {id:5, title:" challenge", content:"make design responsive and add comments in code"}
 ];
