0874fad5af645e2ac5014d944723cb1e8db2cefc
M V K <krishna.mugala9@gmail.com>
2023-11-14 22:03:07 +0530

    changes made

--- Keep-Note/src/app/model/notes.ts
+++ Keep-Note/src/app/model/notes.ts
@@ -2,8 +2,8 @@ import { Note } from './note';
 
 export const Notes:Note[]=[
     {id:1, title:"sample-note", content:"This is a sample note added for testing purpose"},
-    {id:2, title:"Adding data", content:"to add function to calculate final bill amount"},
-    {id:3, title:"Fetch Data", content:"to add function to fetch notes"},
+    {id:2, title:"adding data", content:"to add function to calculate final bill amount"},
+    {id:3, title:"fetch Data", content:"to add function to fetch notes"},
     {id:4, title:"vamshi", content:"code needs to be well-indented"},
-    {id:5, title:" challenge", content:"make design responsive and add comments in code"}
+    {id:5, title:"challenge", content:"make design responsive and add comments in code"}
 ];
